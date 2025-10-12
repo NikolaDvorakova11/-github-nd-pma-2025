8422d0829b6f68a74a39bf63f656ef668885e3a5
Nikola Dvořáková <nikola.dvorakova@tul.cz>
2025-10-12 16:22:30 +0200

    #5 dodělání binding přístupu

--- MyApp004MoreActivities/app/src/main/java/com/example/myapp004moreactivities/MainActivity.kt
+++ MyApp004MoreActivities/app/src/main/java/com/example/myapp004moreactivities/MainActivity.kt
@@ -8,23 +8,26 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.myapp004moreactivities.databinding.ActivityMainBinding
+import com.example.myapp004moreactivities.databinding.ActivitySecondBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
 
-        setContentView(R.layout.activity_main)
+       //Inicializace View Bindingu
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val btnSecondAct = findViewById<Button>( R.id.btnSecondAct)
-        val etNickname = findViewById<EditText>(R.id.etNickname)
-        val etStartNumber = findViewById<EditText>(R.id.etStartNumber)
-        val etAge = findViewById<EditText>(R.id.etAge)
-
-        btnSecondAct.setOnClickListener {
-            val nickname = etNickname.text.toString()
-            val startNumber = etStartNumber.text.toString()
-            val age  = etAge.text.toString()
+       //Odeslání dat do SecondActivity
+        binding.btnSecondAct.setOnClickListener {
+            val nickname = binding.etNickname.text.toString()
+            val startNumber = binding.etStartNumber.text.toString()
+            val age  = binding.etAge.text.toString()
 
             val intent = Intent(this, SecondActivity::class.java)
             intent.putExtra("NICK_NAME",nickname)
