365f0230941f2b7ae564df075426980f1c7f70a5
Nikola Dvořáková <nikola.dvorakova@tul.cz>
2025-11-06 11:13:21 +0100

    #11 příprava pro viewBinding

---
+++ MyApp012aMyNoteHub/app/src/main/java/com/example/myapp012amynotehub/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.myapp012amynotehub
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.myapp012amynotehub.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        //setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    }
+}
