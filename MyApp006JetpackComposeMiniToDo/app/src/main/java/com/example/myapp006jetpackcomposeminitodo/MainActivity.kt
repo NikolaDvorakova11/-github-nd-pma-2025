fc9a7637a04a2269365127209e066f6f055b73ae
Nikola Dvořáková <nikola.dvorakova@tul.cz>
2025-12-04 13:13:53 +0100

    #15 Jetpack - Úkol - přidání nadpisu a zarovnání na střed

--- MyApp006JetpackComposeMiniToDo/app/src/main/java/com/example/myapp006jetpackcomposeminitodo/MainActivity.kt
+++ MyApp006JetpackComposeMiniToDo/app/src/main/java/com/example/myapp006jetpackcomposeminitodo/MainActivity.kt
@@ -6,6 +6,8 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -15,15 +17,29 @@ import androidx.compose.ui.unit.dp
 import com.example.myapp006jetpackcomposeminitodo.ui.theme.MyApp006JetpackComposeMiniToDoTheme
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        enableEdgeToEdge()
         setContent {
             MyApp006JetpackComposeMiniToDoTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    ToDoScreen(modifier = Modifier
-                        .padding(innerPadding) // systémový padding (status bar apod.)
-                        .padding(16.dp) // náš vlastní padding navíc
+                Scaffold(
+                    modifier = Modifier.fillMaxSize(),
+
+                    // ÚKOL – přidaný Top Bar se zarovnání na střed
+                    topBar = {
+                        CenterAlignedTopAppBar(
+                            title = {
+                                Text("Moje To-Do")              // Text uprostřed
+                            }
+                        )
+                    }
+
+
+                ) { innerPadding ->
+                    ToDoScreen(
+                        modifier = Modifier
+                            .padding(innerPadding) // systémový padding (status bar apod.)
                     )
                 }
             }
