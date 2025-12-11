7dd586dccc4866ac00e71035cc0cce012bc2ba0d
Nikola Dvořáková <nikola.dvorakova@tul.cz>
2025-12-11 11:39:05 +0100

    #17 Navigace

---
+++ MyApp016aNavigationDemo/app/src/main/java/com/example/myapp016anavigationdemo/MainActivity.kt
@@ -0,0 +1,55 @@
+package com.example.myapp016anavigationdemo
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.WindowInsetsCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.appbar.MaterialToolbar
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationView
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_main)
+
+        val navHost = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHost.navController
+
+        // Najdeme toolbar
+        val toolbar = findViewById<MaterialToolbar>(R.id.toolbar)
+
+        // Nastavíme ho jako ActionBar
+        setSupportActionBar(toolbar)
+        
+        val drawerLayout = findViewById<DrawerLayout>(R.id.drawerLayout)
+        appBarConfiguration = AppBarConfiguration(navController.graph, drawerLayout)
+
+
+        // Až teď může NavigationUI pracovat s ActionBarem
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+        findViewById<BottomNavigationView>(R.id.bottomNav)
+            .setupWithNavController(navController)
+
+        findViewById<NavigationView>(R.id.drawerNav)
+            .setupWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
+}
